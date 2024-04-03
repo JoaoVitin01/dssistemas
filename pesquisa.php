9e91b164cc0244de3c38280a3096851950f1c5d7
JoaoVitin01 <129520472+JoaoVitin01@users.noreply.github.com>
2024-04-03 14:32:38 -0300

    2.6v

--- pesquisa.php
+++ pesquisa.php
@@ -58,8 +58,11 @@
         <td>$telefone</td>
         <td>$data</td>
         <td>$email</td>
-        <td>
+        <td width=150px>
 
+        <a href='#' class='btn btn-success btn-sm'>Editar</a>
+        <a href='#' class='btn btn-danger btn-sm'>Excluir</a>
+        
         </td>
 
         </tr>";
@@ -70,14 +73,17 @@
     
   </tbody>
 </table>
+    <a href="inicio.php" class="btn btn-info">Voltar ao Inicio</a>
                 </nav>
 
             </div>
         </div>
     </div>
-    <a href="inicio.php" class=" btn btn-info">Voltar ao Inicio</a>
-
+   
+    
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+   
 </body>
 
 </html>
